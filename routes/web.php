e135873c880477d465d2cc60b8a2e82e81c419f4
mirnakusumaa <mirnakusumawati38@gmail.com>
2017-11-14 09:03:05 +0700

    menambahkan laravel

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::prefix('admin')->group(function() {
   Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
   Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
   Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+  Route::resource('manageadmins', 'ManageAdminController');
 
   // Password reset routes
   Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
